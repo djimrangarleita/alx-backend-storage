7a46fe7315b8749ce0d80ec62c7c0e76bd7e3a29
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-31 05:36:01 +0300

    chore: Write strings to redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Redis basic manipulations
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Class that contain methods for redis manip"""
+
+    def __init__(self):
+        """Class initialization"""
+        self._redis = redis.Redis()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store the data passed in param in redis"""
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
