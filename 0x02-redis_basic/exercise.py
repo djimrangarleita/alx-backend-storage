6b3e1bc2f7e42f71a978a2f4344e4bce9eab58e5
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-31 05:58:45 +0300

    fix: Flush the store during initialization

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,6 +13,7 @@ class Cache:
     def __init__(self):
         """Class initialization"""
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the data passed in param in redis"""
