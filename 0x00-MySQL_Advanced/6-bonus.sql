383338250ceac380b3c6247597cafdef4da99955
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-14 09:52:25 +0300

    chore(#6): Create procedure that adds a new correction for a student

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,22 @@
+-- Procedure that adds a new correction for a student based on in params
+-- params: user_id, project_name(create if not exists), score
+-- tables: users, projects, corrections
+DELIMITER //
+
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+	DECLARE project_id INT;
+	SELECT id
+	INTO project_id
+	FROM projects
+	WHERE name = project_name;
+	IF project_id IS NULL THEN
+		INSERT INTO projects (name)
+		VALUES (project_name);
+		SET project_id = LAST_INSERT_ID();
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, project_id, score);
+END;//
+
+DELIMITER ;
